1201ba7c344ff9727a0e16de83f8fbffd1c4c6d4
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 22:31:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
-def square_array(numbers)
- numbers.each do |i|
-   i ** 2
-end 
+# def square_array(numbers)
+# numbers.each do |i|
+#   i ** 2
+# end 
+# end
+
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
